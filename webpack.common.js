3ec7525eb36ffe5a052c86edc03891853d1c9810
alejandro <avenegasc14@gmail.com>
2021-08-29 23:28:26 -0600

    Add webpack HTML configuration

--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,14 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   entry: {
     main: "./src/index.js",
     vendor: "./src/vendor.js",
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src/index.html"),
+    }),
+  ],
 };

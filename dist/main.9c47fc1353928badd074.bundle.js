aa715449604179e3d5ea758822a7d0bff6e027df
alejandro <avenegasc14@gmail.com>
2021-09-06 08:20:34 -0600

    Move parallax CSS to its own file

---
+++ dist/main.9c47fc1353928badd074.bundle.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";const t=document.querySelectorAll("#logo path");for(const o of t)console.log(o.getTotalLength()*(o.getBBox().width/o.getBoundingClientRect().width))})();
--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
 import "./styles/main.scss";
-const paths = document.querySelectorAll("#logo path");
-for (const path of paths) {
-  console.log(
-    path.getTotalLength() *
-      (path.getBBox().width / path.getBoundingClientRect().width)
-  );
-}

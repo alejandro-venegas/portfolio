0906647ee65b8718135f381612cd32a072bcf550
alejandro <avenegasc14@gmail.com>
2021-09-08 15:19:10 -0600

    Some refactoring of html heriarchy

---
+++ src/app/init.js
@@ -0,0 +1,37 @@
+const nav = document.querySelector("nav");
+const spacer = document.querySelector(".empty-spacer");
+
+spacer.setAttribute(
+  "style",
+  `height: calc(100vh - ${nav.clientHeight}px - 10px)`
+);
+// Used to fix parallax on iOS browsers
+if (getOS() === "iOS") {
+  const parallaxWrappers = document.querySelectorAll(".parallax .wrapper");
+  parallaxWrappers.forEach((value) => {
+    value.setAttribute("style", "position: -webkit-sticky");
+  });
+}
+
+function getOS() {
+  const userAgent = window.navigator.userAgent,
+    platform = window.navigator.platform,
+    macosPlatforms = ["Macintosh", "MacIntel", "MacPPC", "Mac68K"],
+    windowsPlatforms = ["Win32", "Win64", "Windows", "WinCE"],
+    iosPlatforms = ["iPhone", "iPad", "iPod"],
+    os = null;
+
+  if (macosPlatforms.indexOf(platform) !== -1) {
+    os = "Mac OS";
+  } else if (iosPlatforms.indexOf(platform) !== -1) {
+    os = "iOS";
+  } else if (windowsPlatforms.indexOf(platform) !== -1) {
+    os = "Windows";
+  } else if (/Android/.test(userAgent)) {
+    os = "Android";
+  } else if (!os && /Linux/.test(platform)) {
+    os = "Linux";
+  }
+
+  return os;
+}

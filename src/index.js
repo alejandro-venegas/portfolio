60e4a4f0038e482c0260ef7fe3ff0e0cf7acbada
alejandro <avenegasc14@gmail.com>
2021-09-10 16:23:52 -0600

    Add face

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 import "./styles/main.scss";
 import "./app/init";
+
+let options = {
+  root: document.querySelector("body"),
+  rootMargin: "0px",
+  threshold: 1,
+};
+const faceSVG = document.querySelector("#svg-face");
+let observer = new IntersectionObserver((entries, observer) => {
+  if (entries[0].isIntersecting) {
+    faceSVG.classList.add("animate");
+  }
+}, options);
+
+observer.observe(faceSVG);
